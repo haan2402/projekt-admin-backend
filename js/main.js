06fdd0ab51c2a851682a834ff5a761977dd3ae6e
Hanna Angeria <haan2402@student.miun.se>
2025-06-01 19:25:52 +0200

    lagt till id i formuläret hade glömt det, gjort js kod för att lägga till och skriva ut nya rätter, ändrat inställning i package json fil för min parcel slutade uppdatera i admin koden

--- js/main.js
+++ js/main.js
@@ -1,21 +1,82 @@
 "use strict";
 
+//om det inte skickas med en token så stannar man på index och logga in
+const token = localStorage.getItem("token");
+if(!token) {
+    window.location.href = "index.html"; 
+} 
+
 const api = 'http://localhost:3000/api/foodItems';
-const form = document.getElementById("add-Form");
 const menuList = document.getElementById("list-food");
 
 //kontroll av token och laddar in data från api
 document.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem("token");
-    if(!token) {
-        window.location.href = "index.html"; //om det inte skickas med en token så stannar man på index och logga in
-    } else {
-        getMenu(token);
+    getMenu();
+
+    const form = document.getElementById('add-Form');
+    if(form) {
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            await registerDish();
+        });
     }
 });
 
+//funktion för att lägga till ny maträtt från formulär
+async function registerDish() {
+
+    //gör variabler av inputfälten
+    const name = document.getElementById('name').value;
+    const description = document.getElementById('description').value;
+    const category = document.getElementById('category').value;
+    const price = document.getElementById('price').value;
+
+    //till meddelande om det är ok eller om något blev fel
+    const message = document.getElementById("message");
+    
+    //gör ett objekt av ny rätt
+    const newFood = { name, description, category, price: parseFloat(price) };
+
+    //kontroll att alla fält fylls i
+    if(!name || !description ||!price || !category) {
+        message.textContent = "Alla fält måste fyllas i";
+        message.style.color = 'red';
+        return;
+    }
+
+    try {
+        const response = await fetch(api, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            },
+            body: JSON.stringify(newFood)
+        });
+
+        //om rätten har lagts till får man ett meddelande
+        if(response.ok) {
+        message.textContent = "Maträtt tillagd i menyn!"
+        message.style.color = 'green';
+        document.getElementById('add-Form').reset();
+        getMenu();
+
+        } else {
+            const errorFood = await response.json();
+            message.textContent = errorFood.message || "Något gick fel..";
+            message.style.color = 'red';
+        }
+
+    } catch (error) {
+        console.error("Gick inte att lägga till rätten..", error);
+        message.textContent = "Gick inte att lägga till rätten..";
+        message.style.color = 'red'; 
+    }
+}
+
+
 //hämtar in maträtterna som finns tillgängliga i menyn
-async function getMenu(token) {
+async function getMenu() {
     try {
         const response = await fetch(api, {
             method: "GET",
@@ -46,8 +107,8 @@ function writeMenu(menu) {
         </div>
         <p class="dish-desc">${item.description}</p>
         <div class="buttons">
-        <button class="edit-Btn" data-id="('${item.id}')">Ändra</button>
-        <button class="delete-Btn" data-id="('${item.id}')">Ta bort</button>
+        <button class="edit-Btn" data-id="${item.id}">Ändra</button>
+        <button class="delete-Btn" data-id="${item.id}">Ta bort</button>
         </div>
         </div> `;
 
